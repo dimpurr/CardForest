986e338d5c45328c98805d162a4f2b98aa486b77
dimpurr <dimpur@live.com>
2023-08-19 00:31:51 +0800

    feat: multi pic res singlt page fin ver

--- cardforest/packages/chrome-extension/content.js
+++ cardforest/packages/chrome-extension/content.js
@@ -1,28 +1,49 @@
 const getResolution = async (pixivId) => {
     try {
-        const response = await fetch(`https://www.pixiv.net/ajax/illust/${pixivId}`);
+        const response = await fetch(`https://www.pixiv.net/ajax/illust/${pixivId}/pages`);
         const data = await response.json();
-        if (data && data.body && data.body.urls && data.body.urls.original) {
-            const imgUrl = data.body.urls.original;
-            const image = new Image();
-            image.src = imgUrl;
-            image.onload = () => {
-                const resolution = { width: image.width, height: image.height };
-                console.log(`Resolution: ${resolution.width} x ${resolution.height}`);
-                localStorage.setItem(`pixiv_${pixivId}_resolution`, JSON.stringify(resolution));
-                displayResolution(resolution);
-            };
+
+        if (data && data.body) {
+            const resolutions = [];
+            for (let item of data.body) {
+                const imgUrl = item.urls.original;
+                const image = new Image();
+                image.src = imgUrl;
+                image.onload = () => {
+                    resolutions.push({ width: image.width, height: image.height });
+                    if (resolutions.length === data.body.length) {
+                        console.log('All resolutions fetched:', resolutions);
+                        localStorage.setItem(`pixiv_${pixivId}_resolution`, JSON.stringify(resolutions));
+                        displayResolution(resolutions);
+                    }
+                };
+            }
         }
     } catch (error) {
         console.error("Error fetching resolution:", error);
     }
 }
 
-const displayResolution = (resolution) => {
+const displayResolution = (resolutions) => {
     const targetDiv = document.querySelector('body');
     if (targetDiv) {
-        const label = targetDiv.querySelector('#pixivResolutionLabel');
-        label.textContent = `${resolution.width} x ${resolution.height}`;
+        const existingLabel = targetDiv.querySelector('#pixivResolutionLabel');
+        if (existingLabel) {
+            existingLabel.remove();
+        }
+
+        const label = document.createElement('div');
+        label.id = 'pixivResolutionLabel';
+        label.style.position = 'fixed';
+        label.style.left = '10px';
+        label.style.top = '10px';
+        label.style.background = 'rgba(255, 255, 255, 0.5)';
+
+        // Display all resolutions vertically
+        const resolutionTexts = resolutions.map(res => `${res.width} x ${res.height}`);
+        label.textContent = resolutionTexts.join('\n');
+
+        targetDiv.appendChild(label);
     }
 }
 
@@ -46,7 +67,6 @@ const displayLoading = () => {
 }
 
 const processPage = () => {
-    alert('111')
     const pixivId = window.location.pathname.split('/')[2];
     if (pixivId) {
         displayLoading();
